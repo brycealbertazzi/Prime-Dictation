2198a28233fffbb7f2cd64303861990be4e7ba60
Bryce Albertazzi <brycealbertazzi@gmail.com>
2019-07-28 16:56:46 -0700

    Buttons Set

--- Prime Dictation/Prime Dictation/ViewController.swift
+++ Prime Dictation/Prime Dictation/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ListenLabel: UIButton!
+    @IBOutlet weak var RecordLabel: UIButton!
+    @IBOutlet weak var SendLabel: UIButton!
+    
+    @IBAction func ListenButton(_ sender: UIButton) {
+    }
+    @IBAction func RecordButton(_ sender: UIButton) {
+    }
+    @IBAction func SendButton(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
