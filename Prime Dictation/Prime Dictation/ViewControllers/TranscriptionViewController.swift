f33cd7c35019ce40c3cd70837c467dc450b4c03d
Bryce Albertazzi <brycealbertazzi@gmail.com>
2025-10-30 19:04:01 -0600

    bottom inset when keyboard is showing

--- Prime Dictation/Prime Dictation/ViewControllers/TranscriptionViewController.swift
+++ Prime Dictation/Prime Dictation/ViewControllers/TranscriptionViewController.swift
@@ -16,13 +16,22 @@ class TranscriptionViewController: UIViewController {
         
         TranscriptionTextBox.text = transcriptText
         
-        //Option to press Done action item on keyboard toolbar to dismiss
+        // Option to press Done action item on keyboard toolbar to dismiss
         addDoneButtonToKeyboard()
         
-        //Option tap to dismiss keyboard
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        tap.cancelsTouchesInView = false   // so taps on buttons still work
-        view.addGestureRecognizer(tap)
+        // Listen for keyboard show/hide to adjust insets
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     func displayAlert(title: String, message: String, handler: (@MainActor () -> Void)? = nil) {
@@ -44,11 +53,40 @@ class TranscriptionViewController: UIViewController {
 
     @objc private func dismissKeyboard() {
         view.endEditing(true)
+        // Save the updated text to the recording manager
         guard let newText = TranscriptionTextBox.text else {
             ProgressHUD.failed("Failed to update transcription")
             return
         }
         recordingManager.UpdateToggledTranscriptionText(newText: newText)
+    }
+    
+    // MARK: - Keyboard handling
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard
+            let userInfo = notification.userInfo,
+            let frameValue = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        else { return }
+        
+        let keyboardFrameInScreen = frameValue.cgRectValue
+        let keyboardFrameInView = view.convert(keyboardFrameInScreen, from: nil)
+        let bottomInset = view.bounds.maxY - keyboardFrameInView.origin.y
+        
+        var insets = TranscriptionTextBox.contentInset
+        insets.bottom = bottomInset + 8   // add a little padding
+        TranscriptionTextBox.contentInset = insets
+        TranscriptionTextBox.scrollIndicatorInsets = insets
+        
+        // make sure the caret is visible
+        if TranscriptionTextBox.isFirstResponder {
+            TranscriptionTextBox.scrollRangeToVisible(TranscriptionTextBox.selectedRange)
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        TranscriptionTextBox.contentInset = .zero
+        TranscriptionTextBox.scrollIndicatorInsets = .zero
     }
     
     @IBAction func BackButton(_ sender: Any) {
