b2acea3baf4275c49931008cb6fe7fd7710ce398
Bryce Albertazzi <brycealbertazzi@gmail.com>
2025-11-03 18:16:14 -0700

    Update version 31

--- Prime Dictation/Prime Dictation/Utilties/PDColors.swift
+++ Prime Dictation/Prime Dictation/Utilties/PDColors.swift
@@ -6,4 +6,5 @@ struct PDColors {
     static let red    = UIColor(red: 0.690, green: 0.227, blue: 0.180, alpha: 1.0) // #B03A2E
     static let purple = UIColor(red: 0.56,  green: 0.27,  blue: 0.68,  alpha: 1.0) // #8E44AD
     static let black  = UIColor(red: 0.173, green: 0.173, blue: 0.180, alpha: 1.0) // Charcoal
+    static let gray = UIColor(red: 0.50, green: 0.55, blue: 0.55, alpha: 1.0) // #7F8C8D
 }
